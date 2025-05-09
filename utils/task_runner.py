febabc8b633234d6e3873a255663c7907e1ab7b9
Iman Chandrasiri <iman.20221928@iit.ac.lk>
2025-05-09 19:16:56 +0530

    Cleaned up main.py updated

---
+++ utils/task_runner.py
@@ -0,0 +1,35 @@
+from utils.customExceptions.customExceptions import TaskProcessingError
+from utils.coreConfig import config
+from utils.logger.loggers import get_logger
+from processManipulation.fileOperations.fileOperations import process_and_validate_file
+from processManipulation.databaseOperations.databaseOperations import get_open_tasks
+
+
+
+logger = get_logger("main")
+
+
+def main():
+    try:
+        tasks = get_open_tasks()
+        logger.info(f"Number of open tasks fetched: {len(tasks)}")
+    except Exception as e:
+        logger.exception("Failed to fetch open tasks.")
+        return
+
+    for task in tasks:
+        task_id = task["_id"]
+        logger.debug(f"Processing task: {task_id}")
+
+        file_upload_seq = task.get("parameters", {}).get("file_upload_seq")
+        if not file_upload_seq:
+            logger.warning(f"Skipping task {task_id} due to missing file_upload_seq.")
+            continue
+
+        try:
+            logger.info(f"Processing task {task_id} with file_upload_seq: {file_upload_seq}")
+            process_and_validate_file(task)
+        except TaskProcessingError as e:
+            logger.error(f"Task {task_id} encountered a processing error: {e}")
+        except Exception as e:
+            logger.exception(f"Unexpected error processing task {task_id}: {e}")
